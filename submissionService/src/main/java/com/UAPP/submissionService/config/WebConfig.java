ad1aa01b3ecbadb841f59879efccbb9bdc52bfde
AniketDhakate007 <142479987+AniketDhakate007@users.noreply.github.com>
2025-08-22 16:07:37 +0530

    Update WebConfig.java

--- submissionService/src/main/java/com/UAPP/submissionService/config/WebConfig.java
+++ submissionService/src/main/java/com/UAPP/submissionService/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173", "http://localhost:3000") // React dev servers
+                .allowedOriginPatterns("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
