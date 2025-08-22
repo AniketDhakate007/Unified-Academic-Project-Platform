be9d94374ddbf62fe44744d5ccd41d93875db23c
AniketDhakate007 <142479987+AniketDhakate007@users.noreply.github.com>
2025-08-22 16:25:08 +0530

    Update ProjectController.java

--- submissionService/src/main/java/com/UAPP/submissionService/controller/ProjectController.java
+++ submissionService/src/main/java/com/UAPP/submissionService/controller/ProjectController.java
@@ -24,7 +24,9 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/projects")
-@CrossOrigin(origins = "https://unified-academic-project-platform.vercel.app")
+@CrossOrigin(origins = "http://localhost:5173",
+    "http://localhost:3000",
+    "https://unified-academic-project-platform.vercel.app")
 public class ProjectController {
 
     @Autowired
